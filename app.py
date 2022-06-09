ddd424e64c5eb7b9a4b3266dd8369a72a54fe05e
Rabbi-Morgan <francismobolaji1990@gmail.com>
2022-06-09 08:50:01 +0100

    ...for the other machine

--- app.py
+++ app.py
@@ -120,6 +120,8 @@ def index():
 def venues():
     # TODO: replace with real venues data.
     #       num_upcoming_shows should be aggregated based on number of upcoming shows per venue.
+
+    data = []
     data = [{
         "city": "San Francisco",
         "state": "CA",
