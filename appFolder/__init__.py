86b7ac0bb41ebbfc27b61c8a42e6de13416bd450
Rabbi-Morgan <francismobolaji1990@gmail.com>
2022-06-10 16:10:54 +0100

    mvc completed

--- appFolder/__init__.py
+++ appFolder/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_moment import Moment
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from . import controller
 
 #----------------------------------------------------------------------------#
 # App Config.
